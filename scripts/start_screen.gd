b1d00aef5fd2ffe1b7a0d672a20f26af3b4352cb
Alexander Jordan <68730459+Alexander-Jordan@users.noreply.github.com>
2024-06-14 16:11:52 +0200

    Quick fix to not listen for inputs all the time

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -5,7 +5,10 @@ var game_started:bool = false
 signal start_game
 
 func _input(event):
-	if event.is_action_pressed("flap"):
+	if not game_started:
+		return
+	
+	if and event.is_action_pressed("flap"):
 		game_started = true
 		start_game.emit()
 		visible = false
