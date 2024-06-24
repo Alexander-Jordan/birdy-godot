a3ed1a302d3dc09720eae702593ce1ede46e22c5
Alexander Jordan <curtjordan93@gmail.com>
2024-06-24 16:34:11 +0200

    Also save when the game is closed down

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,3 +5,10 @@ func _ready():
 
 func setup_game():
 	SaveSystem.load_persisted_nodes()
+
+func _notification(what:int):
+	# make sure to always save before closing down the game
+	# NOTIFICATION_WM_GO_BACK_REQUEST is used for an android back-button
+	if what == NOTIFICATION_WM_CLOSE_REQUEST or what == NOTIFICATION_WM_GO_BACK_REQUEST:
+		SaveSystem.save_persisted_nodes()
+		get_tree().quit()
