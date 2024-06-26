ab5f0d438b055dd3c813979d00582ca05d59d74b
Alexander Jordan <curtjordan93@gmail.com>
2024-06-26 08:52:41 +0200

    Change name of the sound button to sfx button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,7 +4,7 @@ extends VBoxContainer
 
 @onready var start_button:Button = $main_buttons/start_button
 @onready var stats_button:Button = $main_buttons/stats_button
-@onready var sound_button:Button = $settings_buttons/HBoxContainer/sound_button
+@onready var sfx_button:Button = $settings_buttons/HBoxContainer/sfx_button
 
 var bus_index:int
 var is_sound_on = true
@@ -14,21 +14,21 @@ func _ready():
 	bus_index = AudioServer.get_bus_index(bus_name)
 	
 	is_sound_on = SaveSystem.settings_data.audio.sfx
-	set_sound()
+	set_sfx()
 	
 	start_button.pressed.connect(load_main_level)
-	sound_button.pressed.connect(toggle_sound)
+	sfx_button.pressed.connect(toggle_sound)
 
 func load_main_level():
 	get_tree().change_scene_to_file('res://scenes/main.tscn')
 
-func set_sound():
-	sound_button.text = 'Sound on' if is_sound_on else 'Sound off'
+func set_sfx():
+	sfx_button.text = 'SFX on' if is_sound_on else 'SFX off'
 	AudioServer.set_bus_mute(bus_index, !is_sound_on)
 
 func toggle_sound():
 	is_sound_on = !is_sound_on
-	set_sound()
+	set_sfx()
 	
 	SaveSystem.settings_data.audio.sfx = is_sound_on
 	SaveSystem.save_settings()
