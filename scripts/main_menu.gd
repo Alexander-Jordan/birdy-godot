ce38a3cc156426d6fdaf4087c5ee0eace73d33cd
Alexander Jordan <curtjordan93@gmail.com>
2024-06-26 08:12:51 +0200

    Implement muting of sound, make menu reachable from end screen

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,17 @@
 extends VBoxContainer
 
+@export var bus_name:String
+
 @onready var start_button:Button = $main_buttons/start_button
 @onready var stats_button:Button = $main_buttons/stats_button
 @onready var sound_button:Button = $settings_buttons/HBoxContainer/sound_button
 
+var bus_index:int
 var is_sound_on = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	bus_index = AudioServer.get_bus_index(bus_name)
 	start_button.pressed.connect(load_main_level)
 	sound_button.pressed.connect(toggle_sound)
 
@@ -17,3 +21,4 @@ func load_main_level():
 func toggle_sound():
 	is_sound_on = !is_sound_on
 	sound_button.text = 'Sound on' if is_sound_on else 'Sound off'
+	AudioServer.set_bus_mute(bus_index, !is_sound_on)
