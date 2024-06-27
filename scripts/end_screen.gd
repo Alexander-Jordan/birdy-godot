d6bfddd0123f1bf17a7846347ce48ffcbcd51d5d
Alexander Jordan <curtjordan93@gmail.com>
2024-06-27 10:17:02 +0200

    Add button sound

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -4,16 +4,19 @@ extends VBoxContainer
 @onready var menu_button:TextureButton = $HBoxContainer/menu_button
 @onready var score_label = $Control/VBoxContainer/score
 @onready var best_score_label = $Control/VBoxContainer/best_score
+@onready var audio_stream_player = $AudioStreamPlayer
 
 func _ready():
 	restart_button.pressed.connect(restart_game)
 	menu_button.pressed.connect(load_menu)
 
 func restart_game():
+	audio_stream_player.play()
 	# for now, just reload the current scene
 	get_tree().reload_current_scene()
 
 func load_menu():
+	audio_stream_player.play()
 	# for now, just change to the menu scene
 	get_tree().change_scene_to_file('res://scenes/menu.tscn')
 
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,6 +4,7 @@ extends VBoxContainer
 @onready var stats_button:TextureButton = $main_buttons/stats_button
 @onready var sfx_button:TextureButton = $settings_buttons/HBoxContainer/sfx_button
 @onready var music_button:TextureButton = $settings_buttons/HBoxContainer/music_button
+@onready var audio_stream_player:AudioStreamPlayer = $AudioStreamPlayer
 
 var sfx_bus_index:int
 var music_bus_index:int
@@ -12,6 +13,8 @@ var music_bus_index:int
 func _ready():
 	music_bus_index = AudioServer.get_bus_index('music')
 	sfx_bus_index = AudioServer.get_bus_index('sfx')
+	AudioServer.set_bus_mute(music_bus_index, !SaveSystem.settings_data.audio.music)
+	AudioServer.set_bus_mute(sfx_bus_index, !SaveSystem.settings_data.audio.sfx)
 	
 	start_button.pressed.connect(load_main_level)
 	music_button.toggled.connect(toggle_music)
@@ -19,20 +22,19 @@ func _ready():
 	
 	music_button.set_pressed_no_signal(!SaveSystem.settings_data.audio.music)
 	sfx_button.set_pressed_no_signal(!SaveSystem.settings_data.audio.sfx)
-	music_button.toggled.emit(!SaveSystem.settings_data.audio.music)
-	sfx_button.toggled.emit(!SaveSystem.settings_data.audio.sfx)
 
 func load_main_level():
+	audio_stream_player.play()
 	get_tree().change_scene_to_file('res://scenes/main.tscn')
 
 func toggle_music(is_off:bool):
+	audio_stream_player.play()
 	AudioServer.set_bus_mute(music_bus_index, is_off)
-	if SaveSystem.settings_data.audio.music != !is_off:
-		SaveSystem.settings_data.audio.music = !is_off
-		SaveSystem.save_settings()
+	SaveSystem.settings_data.audio.music = !is_off
+	SaveSystem.save_settings()
 
 func toggle_sfx(is_off:bool):
+	audio_stream_player.play()
 	AudioServer.set_bus_mute(sfx_bus_index, is_off)
-	if SaveSystem.settings_data.audio.sfx != !is_off:
-		SaveSystem.settings_data.audio.sfx = !is_off
-		SaveSystem.save_settings()
+	SaveSystem.settings_data.audio.sfx = !is_off
+	SaveSystem.save_settings()
