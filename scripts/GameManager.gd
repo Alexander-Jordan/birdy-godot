5f34869a9bade6b739011a12092a7250abacbe43
Alexander Jordan <curtjordan93@gmail.com>
2024-06-28 11:35:05 +0200

    Improve points and medals by adding last_points and last_medals

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -6,8 +6,6 @@ class_name GameManager
 
 var game_stats_data:GameStatsData = GameStatsData.new()
 var save_file_name:String = 'game_stats_data.tres'
-var score:int = 0
-var medal:int = 0
 var is_movement_stopped:bool = false
 var is_game_over:bool = false
 
@@ -16,6 +14,7 @@ signal movement_stopped
 signal game_over(total_score:int, medal:int, best_score:int)
 
 func save_data(base_path:String):
+	game_stats_data.add_medal()
 	ResourceSaver.save(game_stats_data, base_path + save_file_name)
 
 func load_data(base_path:String):
@@ -23,26 +22,13 @@ func load_data(base_path:String):
 		game_stats_data = ResourceLoader.load(base_path + save_file_name).duplicate(true)
 
 func _on_start_game():
+	game_stats_data.last_score = 0
 	music_player.play()
 
 func _on_pipe_passed():
 	# increment points
-	score += 1
-	score_added.emit(score)
-	
-	# is it a new best score?
-	if score > game_stats_data.best_score:
-		game_stats_data.best_score = score
-	
-	if score >= game_stats_data.point_for_bronze_medal:
-		medal = 1
-	if score >= game_stats_data.point_for_silver_medal:
-		medal = 2
-	if score >= game_stats_data.point_for_gold_medal:
-		medal = 3
-	if score >= game_stats_data.point_for_platinum_medal:
-		medal = 4
-	
+	game_stats_data.last_score += 1
+	score_added.emit(game_stats_data.last_score)
 	point_audio_player.play()
 
 func _on_pipe_collision():
@@ -55,6 +41,5 @@ func _on_ground_collision():
 		movement_stopped.emit()
 	if not is_game_over:
 		music_player.stop()
-		game_over.emit(score, medal, game_stats_data.best_score)
-		game_stats_data.add_medal(medal)
+		game_over.emit(game_stats_data.last_score, game_stats_data.last_medal, game_stats_data.best_score)
 		SaveSystem.save_persisted_nodes()
--- scripts/game_stats_data.gd
+++ scripts/game_stats_data.gd
@@ -6,6 +6,14 @@ const point_for_silver_medal:int = 25
 const point_for_gold_medal:int = 50
 const point_for_platinum_medal:int = 100
 
+@export var last_score:int = 0 :
+	set(value):
+		last_score = value
+		set_last_medal()
+		# is it a new best score?
+		if self.last_score > self.best_score:
+			self.best_score = self.last_score
+@export var last_medal:int = 0
 @export var best_score:int = 0
 @export var bronze_first:String = '-'
 @export var silver_first:String = '-'
@@ -16,20 +24,32 @@ const point_for_platinum_medal:int = 100
 @export var gold_count:int = 0
 @export var platinum_count:int = 0
 
-func add_medal(medal_index:int):
-	if medal_index == 1:
+func set_last_medal():
+	if (self.last_score >= self.point_for_bronze_medal) and (self.last_score < self.point_for_silver_medal):
+		self.last_medal = 1
+	elif (self.last_score >= self.point_for_silver_medal) and (self.last_score < self.point_for_gold_medal):
+		self.last_medal = 2
+	elif (self.last_score >= self.point_for_gold_medal) and (self.last_score < self.point_for_platinum_medal):
+		self.last_medal = 3
+	elif self.last_score >= self.point_for_platinum_medal:
+		self.last_medal = 4
+	else:
+		self.last_medal = 0
+
+func add_medal():
+	if self.last_medal == 1:
 		if bronze_count == 0:
 			bronze_first = Time.get_datetime_string_from_system()
 		bronze_count += 1
-	if medal_index == 2:
+	if self.last_medal == 2:
 		if silver_count == 0:
 			silver_first = Time.get_datetime_string_from_system()
 		silver_count += 1
-	if medal_index == 3:
+	if self.last_medal == 3:
 		if gold_count == 0:
 			gold_first = Time.get_datetime_string_from_system()
 		gold_count += 1
-	if medal_index == 4:
+	if self.last_medal == 4:
 		if platinum_count == 0:
 			platinum_first = Time.get_datetime_string_from_system()
 		platinum_count += 1
